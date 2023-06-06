d39207a5d434a12ca8535d3dd300f84c5e3c42f4
Derek Honrubia <111546379+Dekyut@users.noreply.github.com>
2023-06-06 16:18:29 +0800

    Update AbtMePage1.java

--- LONGEXAM1/src/longexam/AbtMePage1.java
+++ LONGEXAM1/src/longexam/AbtMePage1.java
@@ -46,13 +46,14 @@ public class AbtMePage1 extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		
+		// this puts a text panel where I can put my information
 		JTextPane AbtMeTxtPane1 = new JTextPane();
 		AbtMeTxtPane1.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 29));
 		AbtMeTxtPane1.setText("I am a first-year college student at National University who is a dedicated, hardworking, and creative individual with the goal of creating work and class atmospheres comfortable and fun as well as finishing tasks on time with the utmost greatest quality I can make.");
 		AbtMeTxtPane1.setBounds(100, 214, 455, 218);
 		contentPane.add(AbtMeTxtPane1);
 		
+		//this a label/button that will take the user to the previous page
 		JLabel AbtMe1BackLbl = new JLabel("");
 		AbtMe1BackLbl.addMouseListener(new MouseAdapter() {
 			@Override
@@ -65,6 +66,7 @@ public class AbtMePage1 extends JFrame {
 		AbtMe1BackLbl.setBounds(45, 431, 63, 53);
 		contentPane.add(AbtMe1BackLbl);
 		
+		//this label/button will take the user to the next page
 		JLabel AbtMe1NextLbl = new JLabel("");
 		AbtMe1NextLbl.addMouseListener(new MouseAdapter() {
 			@Override
@@ -77,6 +79,7 @@ public class AbtMePage1 extends JFrame {
 		AbtMe1NextLbl.setBounds(786, 431, 63, 53);
 		contentPane.add(AbtMe1NextLbl);
 		
+		//this contains the background of the design
 		JLabel AbtMeBack1 = new JLabel("");
 		AbtMeBack1.setIcon(new ImageIcon("C:\\Users\\Lovely\\eclipse-workspace\\LONGEXAM1\\src\\longexam\\NewAbtMeBack.png"));
 		AbtMeBack1.setBounds(0, 0, 886, 511);
